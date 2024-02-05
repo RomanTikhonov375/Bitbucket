582b532c247b53c318f365be688205c670b12976
Roman Tikhonov <Roman.Tikhonov375@gmail.com>
2024-02-05 23:39:14 +0300

    feat congig vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-
-  base: '/Bitbucket/'
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/Bitbucket/'
+  : '/',
 })
